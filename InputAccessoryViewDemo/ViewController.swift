4226954e774b8cb9022e69b9607531ab07ffe995
YaXuan Liang <xuan@zoaks.com>
2017-02-23 16:51:56 +0800

    InputAccessoryViewDemo init

---
+++ InputAccessoryViewDemo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  InputAccessoryViewDemo
+//
+//  Created by 梁雅軒 on 2017/2/23.
+//  Copyright © 2017年 zoaks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var textMessage: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textMessage.inputAccessoryView = contentView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textMessage.resignFirstResponder()
+    }
+
+    
+
+}
